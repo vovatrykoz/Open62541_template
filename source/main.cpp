e4d08c8c2d18824dce794b34e123527a138d54f8
vovatrykoz <vovatrykoz@gmail.com>
2024-10-11 16:10:28 +0200

    first commit

---
+++ source/main.cpp
@@ -0,0 +1,9 @@
+#include <open62541/server.h>
+
+int main(void) {
+    UA_Server* server = UA_Server_new();
+    UA_Server_runUntilInterrupt(server);
+    UA_Server_delete(server);
+
+    return 0;
+}
